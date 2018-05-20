d9fda182c3c5ca8343a36d7aa2a629bc0a71bd73
Congmin <congminqi@gmail.com>
2018-05-20 16:23:44 +0800

    test if i have master permission

---
+++ src/main/java/com/rice/array0520_041/Solution_cq4.java
@@ -0,0 +1,14 @@
+package com.rice.array0520_041;
+/*
+* xavier.qiu
+* 5/20/18 4:21 PM
+*/
+
+/**
+ * https://leetcode.com/problems/first-missing-positive/description/
+ */
+class Solution_cq4 {
+    public int firstMissingPositive(int[] nums) {
+        return 1;
+    }
+}
